df445e030998f3fcd612454766652766c3fd2d43
gogugogiugogu <o.piotrowski-08@o2.pl>
2025-07-31 14:18:47 +0100

    car-code.lua

--- car-code.lua
+++ car-code.lua
@@ -1,7 +1,7 @@
 while true do
 	wait()
-	script.Parent:MoveTo(game.Workspace.rat1.Position)
+	script.Parent:MoveTo(game.Workspace.Destination1.Position)
 	script.Parent.MoveToFinished:Wait()
-	script.Parent:MoveTo(game.Workspace.rat2.Position)
+	script.Parent:MoveTo(game.Workspace.Destination2.Position)
 	script.Parent.MoveToFinished:Wait()
 end
